29dd539a278af5e35197f6f94a6bf3c2f2045aa0
Trisha Gee <trisha.gee@gmail.com>
2020-01-24 11:31:22 +0100

    Couple of passing tests and some ideas for more

--- src/main/java/com/mechanitis/sudoku/Grid.java
+++ src/main/java/com/mechanitis/sudoku/Grid.java
@@ -2,7 +2,7 @@ package com.mechanitis.sudoku;
 
 public class Grid {
     Row[] rows = new Row[9];
-    private Column[] column = new Column[9];
+    Column[] columns = new Column[9];
 
     private void init() {
 
--- src/test/java/com/mechanitis/sudoku/GridTest.java
+++ src/test/java/com/mechanitis/sudoku/GridTest.java
@@ -10,17 +10,31 @@ class GridTest {
     private final Grid grid = new Grid();
 
     @Test
-    void test() {
+    void should_have_nine_rows() {
         Assertions.assertEquals(9, grid.rows.length);
     }
 
     @Test
     void should_have_nine_columns() {
-        fail("Not implemented");
+        Assertions.assertEquals(9, grid.columns.length);
     }
 
     @Test
     void should_have_exactly_81_cells() {
         fail("Not implemented");
     }
+
+    // should be able to navigate to row from cell
+    // should be able to navigate to column from cell
+    // should be able to view all items in a row
+    // should be able to view all times in a column
+    // should be able to get to a specific cell (column, row) (maybe microtypes are useful for this?)
+
+    // each row should only have the numbers 1-9
+    // each column should only have the numbers 1-9
+    // each number (1-9) should appear 9 times in the grid
+
+
+    //ideas
+    // is this a good place to try out different testing types, mutation testing for example
 }
