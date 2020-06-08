c570eedd5a600d95a4e740a964f2520843c5fcb8
Trisha Gee <trisha.gee@gmail.com>
2020-06-08 17:15:01 +0200

    Creating a more reusable source of data for Coords tests.

--- src/test/java/com/mechanitis/sudoku/data/GridCoordsTest.java
+++ src/test/java/com/mechanitis/sudoku/data/GridCoordsTest.java
@@ -23,108 +23,14 @@ class GridCoordsTest {
 
     @ParameterizedTest
     @DisplayName("Should convert grid coordinates into box coordinates")
-    @MethodSource("gridCoordinatesToBoxCoordinates")
+    @MethodSource("com.mechanitis.sudoku.data.GridBoxAndPositionData#gridCoordinatesBoxCoordinatesAndPosition")
     void shouldConvertGridCoordinatesIntoBoxCoordinates(final GridCoords gridCoords,
                                                         final BoxCoords expectedBoxCoords) {
+        // when
         var boxCoords = gridCoords.toBoxCoords();
+
+        // then
         assertEquals(expectedBoxCoords.row(), boxCoords.row());
         assertEquals(expectedBoxCoords.column(), boxCoords.column());
     }
-
-    static Stream<Arguments> gridCoordinatesToBoxCoordinates() {
-        return Stream.of(
-                // Top Left Box
-                arguments(new GridCoords(0, 0), new BoxCoords(0, 0)),
-                arguments(new GridCoords(0, 1), new BoxCoords(0, 1)),
-                arguments(new GridCoords(0, 2), new BoxCoords(0, 2)),
-                arguments(new GridCoords(1, 0), new BoxCoords(1, 0)),
-                arguments(new GridCoords(1, 1), new BoxCoords(1, 1)),
-                arguments(new GridCoords(1, 2), new BoxCoords(1, 2)),
-                arguments(new GridCoords(2, 0), new BoxCoords(2, 0)),
-                arguments(new GridCoords(2, 1), new BoxCoords(2, 1)),
-                arguments(new GridCoords(2, 2), new BoxCoords(2, 2)),
-                // Top Centre Box
-                arguments(new GridCoords(0, 3), new BoxCoords(0, 0)),
-                arguments(new GridCoords(0, 4), new BoxCoords(0, 1)),
-                arguments(new GridCoords(0, 5), new BoxCoords(0, 2)),
-                arguments(new GridCoords(1, 3), new BoxCoords(1, 0)),
-                arguments(new GridCoords(1, 4), new BoxCoords(1, 1)),
-                arguments(new GridCoords(1, 5), new BoxCoords(1, 2)),
-                arguments(new GridCoords(2, 3), new BoxCoords(2, 0)),
-                arguments(new GridCoords(2, 4), new BoxCoords(2, 1)),
-                arguments(new GridCoords(2, 5), new BoxCoords(2, 2)),
-                // Top Right Box
-                arguments(new GridCoords(0, 6), new BoxCoords(0, 0)),
-                arguments(new GridCoords(0, 7), new BoxCoords(0, 1)),
-                arguments(new GridCoords(0, 8), new BoxCoords(0, 2)),
-                arguments(new GridCoords(1, 6), new BoxCoords(1, 0)),
-                arguments(new GridCoords(1, 7), new BoxCoords(1, 1)),
-                arguments(new GridCoords(1, 8), new BoxCoords(1, 2)),
-                arguments(new GridCoords(2, 6), new BoxCoords(2, 0)),
-                arguments(new GridCoords(2, 7), new BoxCoords(2, 1)),
-                arguments(new GridCoords(2, 8), new BoxCoords(2, 2)),
-                // Centre Left Box
-                arguments(new GridCoords(3, 0), new BoxCoords(0, 0)),
-                arguments(new GridCoords(3, 1), new BoxCoords(0, 1)),
-                arguments(new GridCoords(3, 2), new BoxCoords(0, 2)),
-                arguments(new GridCoords(4, 0), new BoxCoords(1, 0)),
-                arguments(new GridCoords(4, 1), new BoxCoords(1, 1)),
-                arguments(new GridCoords(4, 2), new BoxCoords(1, 2)),
-                arguments(new GridCoords(5, 0), new BoxCoords(2, 0)),
-                arguments(new GridCoords(5, 1), new BoxCoords(2, 1)),
-                arguments(new GridCoords(5, 2), new BoxCoords(2, 2)),
-                // Centre Centre Box
-                arguments(new GridCoords(3, 3), new BoxCoords(0, 0)),
-                arguments(new GridCoords(3, 4), new BoxCoords(0, 1)),
-                arguments(new GridCoords(3, 5), new BoxCoords(0, 2)),
-                arguments(new GridCoords(4, 3), new BoxCoords(1, 0)),
-                arguments(new GridCoords(4, 4), new BoxCoords(1, 1)),
-                arguments(new GridCoords(4, 5), new BoxCoords(1, 2)),
-                arguments(new GridCoords(5, 3), new BoxCoords(2, 0)),
-                arguments(new GridCoords(5, 4), new BoxCoords(2, 1)),
-                arguments(new GridCoords(5, 5), new BoxCoords(2, 2)),
-                // Centre Right Box
-                arguments(new GridCoords(3, 6), new BoxCoords(0, 0)),
-                arguments(new GridCoords(3, 7), new BoxCoords(0, 1)),
-                arguments(new GridCoords(3, 8), new BoxCoords(0, 2)),
-                arguments(new GridCoords(4, 6), new BoxCoords(1, 0)),
-                arguments(new GridCoords(4, 7), new BoxCoords(1, 1)),
-                arguments(new GridCoords(4, 8), new BoxCoords(1, 2)),
-                arguments(new GridCoords(5, 6), new BoxCoords(2, 0)),
-                arguments(new GridCoords(5, 7), new BoxCoords(2, 1)),
-                arguments(new GridCoords(5, 8), new BoxCoords(2, 2)),
-                // Bottom Left Box
-                arguments(new GridCoords(6, 0), new BoxCoords(0, 0)),
-                arguments(new GridCoords(6, 1), new BoxCoords(0, 1)),
-                arguments(new GridCoords(6, 2), new BoxCoords(0, 2)),
-                arguments(new GridCoords(7, 0), new BoxCoords(1, 0)),
-                arguments(new GridCoords(7, 1), new BoxCoords(1, 1)),
-                arguments(new GridCoords(7, 2), new BoxCoords(1, 2)),
-                arguments(new GridCoords(8, 0), new BoxCoords(2, 0)),
-                arguments(new GridCoords(8, 1), new BoxCoords(2, 1)),
-                arguments(new GridCoords(8, 2), new BoxCoords(2, 2)),
-                // Bottom Centre Box
-                arguments(new GridCoords(6, 3), new BoxCoords(0, 0)),
-                arguments(new GridCoords(6, 4), new BoxCoords(0, 1)),
-                arguments(new GridCoords(6, 5), new BoxCoords(0, 2)),
-                arguments(new GridCoords(7, 3), new BoxCoords(1, 0)),
-                arguments(new GridCoords(7, 4), new BoxCoords(1, 1)),
-                arguments(new GridCoords(7, 5), new BoxCoords(1, 2)),
-                arguments(new GridCoords(8, 3), new BoxCoords(2, 0)),
-                arguments(new GridCoords(8, 4), new BoxCoords(2, 1)),
-                arguments(new GridCoords(8, 5), new BoxCoords(2, 2)),
-                // Bottom Right Box
-                arguments(new GridCoords(6, 6), new BoxCoords(0, 0)),
-                arguments(new GridCoords(6, 7), new BoxCoords(0, 1)),
-                arguments(new GridCoords(6, 8), new BoxCoords(0, 2)),
-                arguments(new GridCoords(7, 6), new BoxCoords(1, 0)),
-                arguments(new GridCoords(7, 7), new BoxCoords(1, 1)),
-                arguments(new GridCoords(7, 8), new BoxCoords(1, 2)),
-                arguments(new GridCoords(8, 6), new BoxCoords(2, 0)),
-                arguments(new GridCoords(8, 7), new BoxCoords(2, 1)),
-                arguments(new GridCoords(8, 8), new BoxCoords(2, 2))
-        );
-    }
-
-
 }
--- src/test/java/com/mechanitis/sudoku/data/PositionTest.java
+++ src/test/java/com/mechanitis/sudoku/data/PositionTest.java
@@ -2,132 +2,18 @@ package com.mechanitis.sudoku.data;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.stream.Stream;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class PositionTest {
+    @SuppressWarnings("unused") // this method uses a more general data source and not all params are required
     @ParameterizedTest
     @DisplayName("Should return the correct index for grid coordinates")
-    @MethodSource("gridCoordinatesAndIndex")
-    void shouldReturnTheCorrectIndexForGridCoordinates(int rowIndex, int columnIndex, int index) {
-        assertEquals(index, Position.indexFromCoords(new GridCoords(rowIndex, columnIndex)));
+    @MethodSource("com.mechanitis.sudoku.data.GridBoxAndPositionData#gridCoordinatesBoxCoordinatesAndPosition")
+    void shouldReturnTheCorrectIndexForGridCoordinates(final GridCoords gridCoords, final BoxCoords boxCoords,
+                                                       final int index, final Position position) {
+        assertEquals(index, Position.indexFromCoords(gridCoords));
     }
-
-    static Stream<Arguments> gridCoordinatesAndIndex() {
-        return Stream.of(
-                arguments(0, 0, 0),
-                arguments(0, 3, 1),
-                arguments(0, 6, 2),
-                arguments(3, 0, 3),
-                arguments(3, 3, 4),
-                arguments(3, 6, 5),
-                arguments(6, 0, 6),
-                arguments(6, 3, 7),
-                arguments(6, 6, 8)
-        );
-    }
-
-    //This can be turned into the general data method - add the index and the position and you can reuse it
-    static Stream<Arguments> gridCoordinatesToBoxCoordinates() {
-        return Stream.of(
-                // Top Left
-                arguments(0, 0, 0, 0),
-                arguments(0, 1, 0, 1),
-                arguments(0, 2, 0, 2),
-                arguments(1, 0, 1, 0),
-                arguments(1, 1, 1, 1),
-                arguments(1, 2, 1, 2),
-                arguments(2, 0, 2, 0),
-                arguments(2, 1, 2, 1),
-                arguments(2, 2, 2, 2),
-                // Top Centre
-                arguments(0, 3, 0, 0),
-                arguments(0, 4, 0, 1),
-                arguments(0, 5, 0, 2),
-                arguments(1, 3, 1, 0),
-                arguments(1, 4, 1, 1),
-                arguments(1, 5, 1, 2),
-                arguments(2, 3, 2, 0),
-                arguments(2, 4, 2, 1),
-                arguments(2, 5, 2, 2),
-                // Top Right
-                arguments(0, 6, 0, 0),
-                arguments(0, 7, 0, 1),
-                arguments(0, 8, 0, 2),
-                arguments(1, 6, 1, 0),
-                arguments(1, 7, 1, 1),
-                arguments(1, 8, 1, 2),
-                arguments(2, 6, 2, 0),
-                arguments(2, 7, 2, 1),
-                arguments(2, 8, 2, 2),
-                // Centre Left
-                arguments(3, 0, 0, 0),
-                arguments(3, 1, 0, 1),
-                arguments(3, 2, 0, 2),
-                arguments(4, 0, 1, 0),
-                arguments(4, 1, 1, 1),
-                arguments(4, 2, 1, 2),
-                arguments(5, 0, 2, 0),
-                arguments(5, 1, 2, 1),
-                arguments(5, 2, 2, 2),
-                // Centre Centre
-                arguments(3, 3, 0, 0),
-                arguments(3, 4, 0, 1),
-                arguments(3, 5, 0, 2),
-                arguments(4, 3, 1, 0),
-                arguments(4, 4, 1, 1),
-                arguments(4, 5, 1, 2),
-                arguments(5, 3, 2, 0),
-                arguments(5, 4, 2, 1),
-                arguments(5, 5, 2, 2),
-                // Centre Right
-                arguments(3, 6, 0, 0),
-                arguments(3, 7, 0, 1),
-                arguments(3, 8, 0, 2),
-                arguments(4, 6, 1, 0),
-                arguments(4, 7, 1, 1),
-                arguments(4, 8, 1, 2),
-                arguments(5, 6, 2, 0),
-                arguments(5, 7, 2, 1),
-                arguments(5, 8, 2, 2),
-                // Bottom Left
-                arguments(6, 0, 0, 0),
-                arguments(6, 1, 0, 1),
-                arguments(6, 2, 0, 2),
-                arguments(7, 0, 1, 0),
-                arguments(7, 1, 1, 1),
-                arguments(7, 2, 1, 2),
-                arguments(8, 0, 2, 0),
-                arguments(8, 1, 2, 1),
-                arguments(8, 2, 2, 2),
-                // Bottom Centre
-                arguments(6, 3, 0, 0),
-                arguments(6, 4, 0, 1),
-                arguments(6, 5, 0, 2),
-                arguments(7, 3, 1, 0),
-                arguments(7, 4, 1, 1),
-                arguments(7, 5, 1, 2),
-                arguments(8, 3, 2, 0),
-                arguments(8, 4, 2, 1),
-                arguments(8, 5, 2, 2),
-                // Bottom Right
-                arguments(6, 6, 0, 0),
-                arguments(6, 7, 0, 1),
-                arguments(6, 8, 0, 2),
-                arguments(7, 6, 1, 0),
-                arguments(7, 7, 1, 1),
-                arguments(7, 8, 1, 2),
-                arguments(8, 6, 2, 0),
-                arguments(8, 7, 2, 1),
-                arguments(8, 8, 2, 2)
-        );
-    }
-
 
 }
