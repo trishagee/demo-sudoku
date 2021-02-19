3a85bc11475659897676f54093c02264d4c5aa40
Trisha Gee <trisha.gee@gmail.com>
2021-02-19 11:10:07 +0100

    First attempt at generator. Test fails intermittently.

--- src/main/java/com/mechanitis/sudoku/Generator.java
+++ src/main/java/com/mechanitis/sudoku/Generator.java
@@ -1,11 +1,31 @@
 package com.mechanitis.sudoku;
 
+import com.mechanitis.sudoku.data.DuplicateValueException;
 import com.mechanitis.sudoku.data.Grid;
+import com.mechanitis.sudoku.data.GridCoords;
+
+import java.util.Random;
 
 // there will be some overlap between the generator and the solver, since both are going to need to figure out if a
 // particular number is allowed in a particular spot.
 public class Generator {
+    private Grid grid;
+
+    public Generator() {
+        grid = new Grid();
+
+        for (int i = 0; i <= 17; i++) {
+            Random random = new Random();
+            try {
+                grid.changeCell(new GridCoords(random.nextInt(9), random.nextInt(9)))
+                        .toValue(random.nextInt(9)+1);
+            } catch (DuplicateValueException e) {
+                //brute force: try again
+            }
+        }
+    }
+
     public Grid getPuzzle() {
-        return new Grid();
+        return grid;
     }
 }
